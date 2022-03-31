6051e55eac5cbdb6e574621f2d454b7bfd648e97
Michael Cooper <cooper@w3.org>
2022-03-31 08:11:08 -0400

    basic config

---
+++ respec-config.js
@@ -0,0 +1,42 @@
+var respecConfig = {
+	noRecTrack: true,
+	tocIntroductory: true,
+	specStatus: "ED",
+	
+	shortName: "wcag2ict",
+	
+	//publishDate:  "",
+	copyrightStart: "2022",
+	license: "document",
+	
+	//previousPublishDate:  "",
+	//previousMaturity:  "WD",
+	prevRecURI: "https://www.w3.org/TR/wcag2ict/",
+	
+	// if there a publicly available Editors Draft, this is the link
+	edDraftURI: "https://w3c.github.io/wcag2ict/",
+	
+	editors:[ {
+		name: "Mary Jo Mueller",
+		mailto: "maryjom@us.ibm.com",
+		company: "IBM",
+		companyURI: "https://www.ibm.com/",
+		w3cid: 46880
+	}],
+	//authors:  [
+	//    { name: "Your Name", url: "http://example.org/",
+	//      company: "Your Company", companyURI: "http://example.com/" },
+	//],
+	
+	/*
+	 alternateFormats: [
+	 { uri: 'wcag21-diff.html', label: "Diff from Previous Recommendation" } ,
+	 { uri: 'wcag21.ps', label: "PostScript version" },
+	 { uri: 'wcag21.pdf', label: "PDF version" }
+	 ],
+	 */
+	group: "ag",
+	github: "w3c/wcag2ict",
+	
+	maxTocLevel: 4
+};
