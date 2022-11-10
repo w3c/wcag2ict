8c1065ca350adc6c1f720db141473f6f1c65fde6
daniel-montalvo <dmontalvo@w3.org>
2022-11-10 18:05:54 +0100

    add editors

--- respec-config.js
+++ respec-config.js
@@ -22,7 +22,18 @@ var respecConfig = {
 		company: "IBM",
 		companyURI: "https://www.ibm.com/",
 		w3cid: 46880
-	}],
+	},{
+		Name: "Chriss Loisselle",
+		company: "Oracle Corporation",
+		companyURI: "http://www.oracle.com/",
+		w3cid: "65246"
+	},{
+		name: "Phil Day",
+		company: "NCR",
+		companyURI: "http://www.ncr.com/",
+		w3cid: "65246"
+	}
+],
 	//authors:  [
 	//    { name: "Your Name", url: "http://example.org/",
 	//      company: "Your Company", companyURI: "http://example.com/" },
