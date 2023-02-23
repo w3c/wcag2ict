d8bfc48f4adc303fa4eb861c3b43b4b74323fb25
Michael Cooper <cooper@w3.org>
2023-02-23 10:57:33 -0500

    fetching JSON

--- wcag2ict.js
+++ wcag2ict.js
@@ -1,1 +1,10 @@
-function fetchWcagInfo() {}
+function fetchWcagInfo() {
+  fetch('http://github/w3c/wcag/guidelines/wcag.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    let wcag = data;
+    console.log(wcag.wcag.principles.principle[0].id);
+  });
+}
