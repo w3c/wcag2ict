fc5cfa1a7ce70817154084e7b008912fac56cb3a
Michael Cooper <cooper@w3.org>
2023-04-12 10:12:19 -0400

    Update headers

--- wcag2ict.js
+++ wcag2ict.js
@@ -37,7 +37,10 @@ function fetchWcagInfo() {
 function prepNode(n) {
   var nid = n.id;
   var nsec = document.querySelector('#' + nid);
-  var nhead = nsec.querySelector('h1, h2, h3, h4, h5, h6').parentNode; // header wrapper
+  var nheader = nsec.querySelector('h1, h2, h3, h4, h5, h6');
+  var nhtxt = nheader.childNodes[nheader.childNodes.length - 1];
+  nhtxt.nodeValue = n.name;
+  var nhead = nheader.parentNode; // header wrapper
   var bq = document.createElement("blockquote");
   bq.innerHTML = n.contenttext;
   nhead.after(bq);
