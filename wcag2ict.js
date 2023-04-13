6668150a68d4a86ddc8b36fc33df47f6c977eaf0
Michael Cooper <cooper@w3.org>
2023-04-13 11:51:34 -0400

    update TOC

--- wcag2ict.js
+++ wcag2ict.js
@@ -30,9 +30,12 @@ function prepNode(n) {
   var nid = n.id;
   var nsec = document.querySelector('#' + nid);
   if (nsec) {
-    var nheader = nsec.querySelector('h1, h2, h3, h4, h5, h6');
-    var nhtxt = nheader.childNodes[nheader.childNodes.length - 1];
-    nhtxt.nodeValue = n.name;
+    var nheader = nsec.querySelector('h1, h2, h3, h4, h5, h6'); // header element of section
+    var nhtxt = nheader.childNodes[nheader.childNodes.length - 1]; // last node of the header is the text content
+    nhtxt.nodeValue = n.name; // update header text
+    var tocitem = document.querySelector('a[class="tocxref"][href="#' + nid + '"]'); // get the TOC item
+    var tocitemtxt = tocitem.childNodes[tocitem.childNodes.length -1]; // last child is the text
+    tocitemtxt.nodeValue = n.name; // update toc text
     var nhead = nheader.parentNode; // header wrapper
     var bq = document.createElement("blockquote");
     bq.innerHTML = n.contenttext;
