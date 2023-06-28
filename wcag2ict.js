1cab373691b0fc3167da3433ccddebea27dd2cdb
Michael Cooper <cooper@w3.org>
2023-06-28 08:14:03 -0400

    get working with updated json structure

--- wcag2ict.js
+++ wcag2ict.js
@@ -1,75 +1,81 @@
 function fetchWcagInfo() {
-  fetch('wcag.json')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let wcag = data;
-    
-    wcag.guidelines.principle.forEach(function(princ) {
-      prepPrinc(princ);
-    });
-    wcag.guidelines.term.forEach(function(term) {
-      prepTerm(term);
-    });
-  });
+	fetch('wcag.json').then((response) => {
+		return response.json();
+	}).then((data) => {
+		let wcag = data;
+		
+		wcag.guidelines[0].principles.forEach(function (princ) {
+			prepPrinc(princ);
+		});
+		/*
+		 wcag.guidelines[1].terms.forEach(function(term) {
+		 prepTerm(term);
+		 });
+		 *  */
+	});
 }
 
 function prepPrinc(princ) {
-  prepSec(princ, "principle");
-    
-  if (Array.isArray(princ.guideline)) {
-    princ.guideline.forEach(function(gl) {
-      prepGl(gl);
-    });
-  } else {
-    prepGl(princ.guideline);
-  }
+	prepSec(princ, "principle");
+	
+	princ.guidelines.forEach(function (gl) {
+		prepGl(gl);
+	});
 }
 
 function prepGl(gl) {
-  prepSec(gl, "guideline");
-  
-  if (Array.isArray(gl["success-criterion"])) {
-    gl["success-criterion"].forEach(function(sc) {
-      prepSc(sc);
-    });
-  } else {
-    prepSc(gl["success-criterion"]);
-  };
+	prepSec(gl, "guideline");
+	
+	gl.successcriteria.forEach(function (sc) {
+		prepSc(sc);
+	});
 }
 
 function prepSc(sc) {
-  prepSec(sc, "sc");
+	prepSec(sc, "sc");
 }
 
 function prepTerm(term) {
-  var tid = term.id;
-  prepSec(term, "term");
+	var tid = term.id;
+	prepSec(term, "term");
 }
 
 function prepSec(n, type) {
-  var nid = n.id;
-  var nsec = document.querySelector('#' + nid);
-  if (nsec) {
-    var nname = Array.isArray(n.name) ? n.name[0] : n.name;
-    var nheader = nsec.querySelector('h1, h2, h3, h4, h5, h6'); // header element of section
-    var nhtxt = nheader.childNodes[nheader.childNodes.length - 1]; // last node of the header is the text content
-    nhtxt.nodeValue = nname; // update header text
-    if (type != "term") {
-      var tocitem = document.querySelector('a[class="tocxref"][href="#' + nid + '"]'); // get the TOC item
-      var tocitemtxt = tocitem.childNodes[tocitem.childNodes.length -1]; // last child is the text
-      tocitemtxt.nodeValue = nname; // update toc text
-    }
-    var nhead = nheader.parentNode; // header wrapper
-    var bq = document.createElement("blockquote");
-    bq.setAttribute("class", "wcag-quote");
-    if (type == "term") bq.innerHTML = n.definitiontext;
-    else bq.innerHTML = n.contenttext;
-    nhead.after(bq);
-  }
+	var nid = n.id.split(":")[1];
+	console.log (nid);
+	var nsec = document.querySelector('#' + nid);
+	var nname;
+	if (nsec) {
+		console.log("1");
+		if (type == "term") {
+			nname = Array.isArray(n.name) ? n.name[0]: n.name;
+		} else {
+			nname = n.handle;
+			console.log(nname);
+			// get the TOC item
+			var tocitem = document.querySelector('a[class="tocxref"][href="#' + nid + '"]');
+			// last child is the text
+			var tocitemtxt = tocitem.childNodes[tocitem.childNodes.length -1];
+			// update toc text
+			tocitemtxt.nodeValue = nname;
+		}
+		// header element of section
+		var nheader = nsec.querySelector('h1, h2, h3, h4, h5, h6');
+		// last node of the header is the text content
+		var nhtxt = nheader.childNodes[nheader.childNodes.length - 1];
+		// update header text
+		nhtxt.nodeValue = nname;
+		
+		// header wrapper
+		var nhead = nheader.parentNode;
+		var bq = document.createElement("blockquote");
+		bq.setAttribute("class", "wcag-quote");
+		if (type == "term") bq.innerHTML = n.definition; 
+		else bq.innerHTML = n.content;
+		nhead.after(bq);
+	}
 }
 
 function postRespec() {
-  fetchWcagInfo();
+	fetchWcagInfo();
 }
