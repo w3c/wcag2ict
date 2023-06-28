c6c0d9d310de581a2141dc3d5ceff5729adc28c3
Michael Cooper <cooper@w3.org>
2023-06-28 09:42:46 -0400

    get terms working

--- wcag2ict.js
+++ wcag2ict.js
@@ -7,16 +7,14 @@ function fetchWcagInfo() {
 		wcag.guidelines[0].principles.forEach(function (princ) {
 			prepPrinc(princ);
 		});
-		/*
 		 wcag.guidelines[1].terms.forEach(function(term) {
 		 prepTerm(term);
 		 });
-		 *  */
 	});
 }
 
 function prepPrinc(princ) {
-	prepSec(princ, "principle");
+	prepSec(princ);
 	
 	princ.guidelines.forEach(function (gl) {
 		prepGl(gl);
@@ -24,7 +22,7 @@ function prepPrinc(princ) {
 }
 
 function prepGl(gl) {
-	prepSec(gl, "guideline");
+	prepSec(gl);
 	
 	gl.successcriteria.forEach(function (sc) {
 		prepSc(sc);
@@ -35,30 +33,18 @@ function prepSc(sc) {
 	prepSec(sc, "sc");
 }
 
-function prepTerm(term) {
-	var tid = term.id;
-	prepSec(term, "term");
-}
-
-function prepSec(n, type) {
+function prepSec(n) {
 	var nid = n.id.split(":")[1];
 	console.log (nid);
 	var nsec = document.querySelector('#' + nid);
-	var nname;
 	if (nsec) {
-		console.log("1");
-		if (type == "term") {
-			nname = Array.isArray(n.name) ? n.name[0]: n.name;
-		} else {
-			nname = n.handle;
-			console.log(nname);
-			// get the TOC item
-			var tocitem = document.querySelector('a[class="tocxref"][href="#' + nid + '"]');
-			// last child is the text
-			var tocitemtxt = tocitem.childNodes[tocitem.childNodes.length -1];
-			// update toc text
-			tocitemtxt.nodeValue = nname;
-		}
+		var nname = n.handle;
+		// get the TOC item
+		var tocitem = document.querySelector('a[class="tocxref"][href="#' + nid + '"]');
+		// last child is the text
+		var tocitemtxt = tocitem.childNodes[tocitem.childNodes.length -1];
+		// update toc text
+		tocitemtxt.nodeValue = nname;
 		// header element of section
 		var nheader = nsec.querySelector('h1, h2, h3, h4, h5, h6');
 		// last node of the header is the text content
@@ -70,8 +56,36 @@ function prepSec(n, type) {
 		var nhead = nheader.parentNode;
 		var bq = document.createElement("blockquote");
 		bq.setAttribute("class", "wcag-quote");
-		if (type == "term") bq.innerHTML = n.definition; 
-		else bq.innerHTML = n.content;
+		bq.innerHTML = n.content;
+		nhead.after(bq);
+	}
+}
+
+function prepTerm(n) {
+	var nid = n.id;
+	console.log (nid);
+	var nsec = document.querySelector('#' + nid);
+	if (nsec) {
+		var nname = n.name[0];
+		// get the TOC item
+		var tocitem = document.querySelector('a[class="tocxref"][href="#' + nid + '"]');
+		// last child is the text
+		var tocitemtxt = tocitem.childNodes[tocitem.childNodes.length -1];
+		// update toc text
+		tocitemtxt.nodeValue = nname;
+
+		// header element of section
+		var nheader = nsec.querySelector('h1, h2, h3, h4, h5, h6');
+		// last node of the header is the text content
+		var nhtxt = nheader.childNodes[nheader.childNodes.length - 1];
+		// update header text
+		nhtxt.nodeValue = nname;
+		
+		// header wrapper
+		var nhead = nheader.parentNode;
+		var bq = document.createElement("blockquote");
+		bq.setAttribute("class", "wcag-quote");
+		bq.innerHTML = n.definition; 
 		nhead.after(bq);
 	}
 }
